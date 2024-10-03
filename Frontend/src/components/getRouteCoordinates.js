9bab5c64201566c6bfaf5e8b73d6164fcf067df1
sheikhfaris86 <sheikhfaris86@gmail.com>
2024-10-03 16:53:33 +0530

    Made a separate frontend folder

---
+++ Frontend/src/components/getRouteCoordinates.js
@@ -0,0 +1,30 @@
+import polyline from "@mapbox/polyline";
+
+const API_KEY="9sXz2CALz06ZFOenz2vXqZ9BWAn3JCZvUjqe09mN"
+const originLat="13.08374"
+const originLong="77.48446"
+const destLat="13.08444"
+const destLong="77.49033"
+
+const data=await fetch(`https://api.olamaps.io/routing/v1/directions?origin=${originLat},${originLong}&destination=${destLat},${destLong}&api_key=${API_KEY}`,{
+    method:"POST",
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      }}
+)
+.then((response)=>{
+    // console.log(response)
+    return response.json();
+})
+.then((data)=>{
+    // console.log(data)
+    return data;
+})
+
+const route_polyline=data.routes[0].overview_polyline;
+console.log(route_polyline);
+const cordinates=polyline.decode(route_polyline)
+console.log(cordinates);
+
+export default cordinates;
+// coordinates is an 2d array of cordinates pon the road.
--- src/components/geoline.js
+++
@@ -1,4 +0,0 @@
-import polyline from "@mapbox/polyline";
-
-const temp=polyline.decode("wkznAau|wMGWQ}@Os@UkAQu@Mq@Mo@Ae@AOAaAGuCCuAGeC@a@DuCFiCe@C",5)
-console.log(temp)
