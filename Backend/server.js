816e1aa5f8e00bc63e9f1d24aa3584b40a2a2c10
sheikhfaris86 <sheikhfaris86@gmail.com>
2024-10-03 17:22:20 +0530

    made a basic server which sends coordinates of polyline

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors";
+import {cordinates} from "./dataProcessing/getRouteCoordinates.js";
+const app=express();
+const PORT = 4331;
+
+
+app.use(cors())
+// format to write in browser http://localhost:4331/api/navigation
+
+app.get("/api/navigation",(req,res)=>{
+    res.json(cordinates)
+})
+app.listen(PORT,()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+})
